47f25e79a729bc39dc66693bc49914ff07fbae8e
matt-hogan <mrhogan103@gmail.com>
2021-09-17 13:34:52 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+// Question 1
+let myNumber;
+
+// Question 2
+myNumber = 5;
+
+// Question 3
+let myOtherNumber = 5;
+
+// Question 4
+let myTotal = myNumber * myOtherNumber;
+
+// Question 5
+let myName = "";
+let myCombo = String(myNumber + myOtherNumber);
+
+// Question 6
+let myOtherCombo = myNumber + myOtherNumber;
+
+// Question 7
+let myRemainder = myNumber % 5;
+
+// Question 8
+let i = 2;
+
+// Question 9
+let int = 4;
+
+// Question 10
+let myBoolean = false;
